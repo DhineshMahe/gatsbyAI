82a8debc0e4c894f4935b906b4a4ac2d6f18dbc8
Dhinesh-T2S <dhinesh.m@touch2success.com>
2020-04-18 03:25:12 +0530

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 
-
 import Info from "../components/Home/Info"
 
 import BackgroundSlider from "gatsby-image-background-slider"
